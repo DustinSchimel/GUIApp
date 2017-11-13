e08699cf8ce151380410347f4544658ce395db7d
Dustin Schimel <dustin.schimel@gmail.com>
2017-11-13 13:20:35 -0700

    Added a listener that checks for when the button is pressed, and when it is it will change color randomly

--- src/gui/view/GUIPanel.java
+++ src/gui/view/GUIPanel.java
@@ -5,6 +5,9 @@ import java.awt.Color;
 import javax.swing.JButton;
 import javax.swing.SpringLayout;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class GUIPanel extends JPanel
 {
 	private GUIAppController appController;
@@ -20,6 +23,7 @@ public class GUIPanel extends JPanel
 		
 		setupPanel();
 		setupLayout();
+		setupListeners();
 	}
 
 	/*
@@ -45,6 +49,22 @@ public class GUIPanel extends JPanel
 	
 	private void setupListeners()
 	{
-		
+		firstButton.addActionListener(new ActionListener()
+		{
+				public void actionPerformed(ActionEvent click)
+				{
+						changeScreenColor();
+				}
+		});
 	}
+	
+	private void changeScreenColor()
+	{
+		int red = (int) (Math.random() * 256);
+		int green = (int) (Math.random() * 256);
+		int blue = (int) (Math.random() * 256);
+		
+		this.setBackground(new Color(red, green, blue));
+	}
+		
 }
