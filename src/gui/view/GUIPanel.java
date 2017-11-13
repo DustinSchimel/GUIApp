d6cb6ca98dfd2e0364d0deabdcfdabd55565b34c
Dustin Schimel <dustin.schimel@gmail.com>
2017-11-13 12:58:32 -0700

    Added more panel methods and setupLayout GUI settings

--- src/gui/view/GUIPanel.java
+++ src/gui/view/GUIPanel.java
@@ -8,11 +8,43 @@ import javax.swing.SpringLayout;
 public class GUIPanel extends JPanel
 {
 	private GUIAppController appController;
+	private JButton firstButton;
+	private SpringLayout appLayout;
 	
 	public GUIPanel(GUIAppController appController)
 	{
 		super();
 		this.appController = appController;
+		firstButton = new JButton("Click on the button");
+		appLayout = new SpringLayout();
+		
+		setupPanel();
+		setupLayout();
 	}
 
+	/*
+	 * Used to add all components to the screen and install a layout manager.
+	 * Also set color :D
+	 */
+	private void setupPanel()
+	{
+		this.setBackground(Color.GREEN);
+		this.setLayout(appLayout);
+		this.add(firstButton);
+	}
+	
+	/*
+	 * Used to hold the constraint positioning for SpringLayout.
+	 * AKA a giant dumping ground for all the xxxx.putConstraint(....) lines.
+	 */
+	private void setupLayout()
+	{
+		appLayout.putConstraint(SpringLayout.SOUTH, firstButton, -134, SpringLayout.SOUTH, this);
+		appLayout.putConstraint(SpringLayout.EAST, firstButton, -138, SpringLayout.EAST, this);
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
 }
